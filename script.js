c3e8a9ca2f38a502337a9ef4d3c9815d44cbc545
danger-3003 <naremsumanth@gmail.com>
2023-11-05 08:14:11 +0530

    added firebase

---
+++ script.js
@@ -0,0 +1,47 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDxjbh79osPSMc38e6JZze0ChlRb3py1Wg",
+    authDomain: "new-register-9c16a.firebaseapp.com",
+    databaseURL: "https://new-register-9c16a-default-rtdb.firebaseio.com",
+    projectId: "new-register-9c16a",
+    storageBucket: "new-register-9c16a.appspot.com",
+    messagingSenderId: "659966685778",
+    appId: "1:659966685778:web:2d3448e1aa64136bd6e58b",
+    measurementId: "G-J5CPYBY2WR"
+  };
+
+// initializeApp
+firebase.initializeApp(firebaseConfig);
+
+// reference for db
+var registerformdb = firebase.database().ref('new-register');
+
+var form=document.getElementById("form").addEventListener('submit',(e)=>
+{
+    e.preventDefault();
+    var fname = getelement('f_name');
+    var lname = getelement('l_name');
+    var faname = getelement("fa_name");
+    var dob = getelement("dob");
+    var email = getelement("email");
+    var pass = getelement("pass");
+
+    addvalues(fname,lname,faname,dob,email,pass);
+    
+})
+const getelement = (id) =>
+{
+    return document.getElementById(id).value;
+}
+const addvalues = (fname, lname, faname, dob, email, pass) =>
+{
+    var updatedform = registerformdb.push();
+    updatedform.set({
+        fname:fname,
+        lname:lname,
+        faname:faname,
+        dob:dob,
+        email:email,
+        pass:pass,
+    });
+
+}
